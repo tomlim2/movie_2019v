9c7f41425e5d3710a48bf65e71082aa62961131f
Younsoo Tom Lim <tomandlim@gmail.com>
2021-08-26 17:50:14 +0900

    Creating github rep

---
+++ README.md
@@ -0,0 +1,3 @@
+# Movie App 2019v
+
+React JS Fundmental (2019 updated)
---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import "./App.css"
+
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+    this.setState({ movies, isLoading: false });
+  }//es6
+  componentDidMount(){
+    this.getMovies();
+  }
+  render(){
+    const { isLoading, movies } = this.state; //es6?
+    return (
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className="loader__text">로딩중</span>  
+        </div>
+      ) : (
+        <div className="movies">
+          {movies.map(movie => (
+          <Movie
+            key={movie.id}
+            id={movie.id} 
+            year={movie.year} 
+            title={movie.title} 
+            summary={movie.summary} 
+            poster={movie.medium_cover_image} 
+            genres={movie.genres} 
+          />
+          ))}
+        </div>
+        )}
+    </section>
+    )
+  }
+};
+export default App;
